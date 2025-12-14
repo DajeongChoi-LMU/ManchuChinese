9a0b96690a5511d6eda07bf48561c688094a0596
DajeongChoi <choidajeong90@gmail.com>
2025-12-14 12:19:34 +0900

    Create editions.js

---
+++ editions.js
@@ -0,0 +1,16 @@
+window.EDITIONS = [
+  {
+    key: "sbb_lunyu",
+    title: "論語 上 (SBB, 1691?)",
+    year: "1691",
+    manifest: "https://dajeongchoi-lmu.github.io/ManchuChinese/3306131067.manifest.json",
+    textUrl: "" // 전사 파일이 있으면 texts/xxx.html 등으로 지정
+  },
+  {
+    key: "sample2",
+    title: "論語 上 (예시 판본, 1755)",
+    year: "1755",
+    manifest: "https://example.org/iiif/manifest.json",
+    textUrl: ""
+  }
+];
