0edeb074697c6f4b3bbf8745eb32885bcbf0f221
DajeongChoi <choidajeong90@gmail.com>
2025-12-14 12:26:03 +0900

    Update editions.js

--- editions.js
+++ editions.js
@@ -1,16 +1,16 @@
 window.EDITIONS = [
   {
-    key: "sbb_lunyu",
-    title: "論語 上 (SBB, 1691?)",
+    key: "japan_sishu_1691",
+    title: "新刻滿漢字四書 (1691)",
     year: "1691",
-    manifest: "https://dajeongchoi-lmu.github.io/ManchuChinese/3306131067.manifest.json",
+    manifest: "https://dl.ndl.go.jp/api/iiif/2551975/manifest.json",
     textUrl: "" 
   },
   {
-    key: "sample2",
-    title: "論語 上 (예시 판본, 1755)",
+    key: "berlin_sishu_1755",
+    title: "御製繙譯四書 (1755)",
     year: "1755",
-    manifest: "https://example.org/iiif/manifest.json",
+    manifest: "https://dajeongchoi-lmu.github.io/ManchuChinese/3306131067.manifest.json",
     textUrl: ""
   }
 ];
